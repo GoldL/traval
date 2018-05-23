2d227852fc4f4081617e5552c7c76c4aafdd61e0
林金鼎 <linjinding@MacBook-Pro.local>
2018-05-23 22:21:30 +0800

    project init

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,9 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'styles': resolve('src/assets/styles'),
+      'components': resolve('src/components'),
+      'pages': resolve('src/pages')
     }
   },
   module: {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
