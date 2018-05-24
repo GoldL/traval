e8cd6be7139ba66fe5bfb461eb9c285a7a3d5537
林金鼎 <linjinding@MacBook-Pro.local>
2018-05-25 00:13:34 +0800

    icons-style

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png" />
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles'
+.icons
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 50%
+  .icon
+    position relative
+    float left
+    width 25%
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        margin 0  auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color $darkTextColor
+</style>
