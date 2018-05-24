c23db2188cbbdf3ab211ef02307d6339761dbb0b
林金鼎 <linjinding@MacBook-Pro.local>
2018-05-25 00:41:46 +0800

    icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(slide, index) in pages" :key="index">
+        <div class="icon" v-for="(item, i) in slide" :key="i">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
@@ -14,7 +18,20 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
-
+      iconsList: []
+    }
+  },
+  computed: {
+    pages () {
+      let pages = []
+      this.iconsList.forEach((item, index) => {
+        let page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return []
     }
   }
 }
@@ -22,35 +39,35 @@ export default {
 
 <style lang="stylus" scoped>
 @import '~styles/varibles'
-.icons
-  overflow hidden
-  width 100%
+@import '~styles/mixins'
+.icons >>> .swiper-container
   height 0
   padding-bottom 50%
-  .icon
-    position relative
-    float left
-    width 25%
-    padding-bottom 25%
-    .icon-img
-      position absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
-      .icon-img-content
-        display block
-        margin 0  auto
-        height 100%
-    .icon-desc
-      position absolute
-      left 0
-      right 0
-      bottom 0
-      height .44rem
-      line-height .44rem
-      text-align center
-      color $darkTextColor
+.icon
+  position relative
+  float left
+  width 25%
+  padding-bottom 25%
+  .icon-img
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom .44rem
+    box-sizing border-box
+    padding .1rem
+    .icon-img-content
+      display block
+      margin 0  auto
+      height 100%
+  .icon-desc
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    height .44rem
+    line-height .44rem
+    text-align center
+    color $darkTextColor
+    ellipsis()
 </style>
