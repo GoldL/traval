00b0f0a243dfb6bafe0f1e6a4c8692f94dabfae1
iGolden <136895062@qq.com>
2018-06-08 00:25:42 +0800

    add city-header

---
+++ src/pages/city/City.vue
@@ -0,0 +1,17 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'city',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
