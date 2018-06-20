469915c3cd1f1ee430821fb411ae46bb30f967bf
iGolden <136895062@qq.com>
2018-06-20 23:57:34 +0800

    city-search-logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,6 +3,21 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+      >
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,7 +26,19 @@ export default {
   name: 'CitySearch',
   data () {
     return {
-      keyword: ''
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        // const result = []
+      }, 100)
     }
   }
 }
@@ -31,5 +58,19 @@ export default {
     line-height: .62rem
     text-align: center
     border-radius: .06rem
+    color: #666
+.search-content
+  z-index: 1
+  overflow: hidden
+  position: absolute
+  top: 1.58rem
+  left: 0
+  right: 0
+  bottom: 0
+  background: #eee
+  .search-item
+    line-height: .62rem
+    padding-left: .2rem
+    background: #fff
     color: #666
 </style>
