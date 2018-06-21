28578a933afc86db388fe9c953298bef0a9aeed8
GoldL <136895062@qq.com>
2018-06-21 10:17:26 +0800

    finish city

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -14,7 +14,7 @@
           v-for="item of list"
           :key="item.id"
           @click="handleCityClick(item.name)"
-        >
+        >{{item.name}}</li>
         <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
       </ul>
     </div>
@@ -24,6 +24,9 @@
 <script>
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data () {
     return {
       keyword: '',
@@ -31,14 +34,36 @@ export default {
       timer: null
     }
   },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
   watch: {
     keyword () {
       if (this.timer) {
         clearTimeout(this.timer)
       }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
       this.timer = setTimeout(() => {
-        // const result = []
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
       }, 100)
+    }
+  },
+  methods: {
+    handleCityClick () {
+
     }
   }
 }
@@ -47,30 +72,30 @@ export default {
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 .search
-  height: .72rem
-  padding: 0 .1rem
-  background: $bgColor
+  height 0.72rem
+  padding 0 0.1rem
+  background $bgColor
   .search-input
-    box-sizing: border-box
-    width: 100%
-    height: .62rem
-    padding: 0 .1rem
-    line-height: .62rem
-    text-align: center
-    border-radius: .06rem
-    color: #666
+    box-sizing border-box
+    width 100%
+    height 0.62rem
+    padding 0 0.1rem
+    line-height 0.62rem
+    text-align center
+    border-radius 0.06rem
+    color #666
 .search-content
-  z-index: 1
-  overflow: hidden
-  position: absolute
-  top: 1.58rem
-  left: 0
-  right: 0
-  bottom: 0
-  background: #eee
+  z-index 1
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background #eee
   .search-item
-    line-height: .62rem
-    padding-left: .2rem
-    background: #fff
-    color: #666
+    line-height 0.62rem
+    padding-left 0.2rem
+    background #fff
+    color #666
 </style>
